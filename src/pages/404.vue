ba6f34afca1e2aecc7306b4ac1a8c0351b16119b
Alexpad25 <de.alex25@yandex.ru>
2021-04-03 07:31:31 +0300

    sketch gitview

---
+++ src/pages/404.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="container">
+    <div class="wrapper-content wrapper-content--fixed">
+      <section>
+        <h1>Page n't found!</h1>
+        <p>Go to <router-link to="/" class="link">Home!</router-link></p>
+      </section>
+    </div>
+  </div>
+</template>
